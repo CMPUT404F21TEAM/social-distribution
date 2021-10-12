9937bb3a89fa303baa7e770ac5d5cc49a0d4fe6f
Olivier Vadiavaloo <vadiaval@ualberta.ca>
2021-10-11 21:49:35 -0400

    Separate modal and form and post creation

--- code/socialDistribution/templatetags/post.py
+++ code/socialDistribution/templatetags/post.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.inclusion_tag('tagtemplates/post.html')
 def card_post(post):
     return {'post': post}
+
+@register.inclusion_tag('tagtemplates/post_form.html')
+def post_form():
+    return {}
