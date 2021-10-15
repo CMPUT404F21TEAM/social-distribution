0477d077872b38f32bc098dfcb034543ec34c6a0
Diego Becerra <diego.enrique.becerra@gmail.com>
2021-10-14 22:58:55 -0600

    Added routes

--- code/api/urls.py
+++ code/api/urls.py
@@ -4,4 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('authors/', views.authors, name='authors'),
+    path('authors/<author_id>/', views.author, name='author'),
+    path('authors/<author_id>/followers/', views.followers, name='followers'),
+    path('authors/<author_id>/liked/', views.liked, name='liked'),
+    path('authors/<author_id>/posts/<post_id>/', views.post, name='post'),
+    path('authors/<author_id>/posts/<post_id>/likes/', views.post_likes, name='post_likes'),
+    path('authors/<author_id>/posts/<post_id>/comments/', views.post_comments, name='post_comments'),
+    path('authors/<author_id>/posts/<post_id>/comments/<comment_id>/likes/', views.comment_likes, name='comment_likes'),
+    path('authors/<author_id>/inbox/', views.inbox, name='inbox'),
 ]
--- code/api/views.py
+++ code/api/views.py
@@ -5,3 +5,30 @@ from django.shortcuts import render
 
 def index(request): 
     return HttpResponse("Welcome to the Social Distribution API", status=200)
+
+def authors(request):
+    return HttpResponse("This is the authors/ endpoint")
+
+def author(request, author_id):
+    return HttpResponse("This is the authors/aid/ endpoint")
+
+def followers(request, author_id):
+    return HttpResponse("This is the authors/aid/followers/ endpoint")
+
+def liked(request, author_id):
+    return HttpResponse("This is the authors/aid/liked/ endpoint")
+
+def post(request, author_id, post_id):
+    return HttpResponse("This is the authors/aid/posts/pid/ endpoint")
+
+def post_likes(request, author_id, post_id):
+    return HttpResponse("This is the authors/aid/posts/pid/likes/ endpoint")
+
+def post_comments(request, author_id, post_id):
+    return HttpResponse("This is the authors/aid/posts/pid/comments/ endpoint")
+
+def comment_likes(request, author_id, post_id, comment_id):
+    return HttpResponse("This is the authors/aid/posts/pid/comments/cid/likes/ endpoint")
+
+def inbox(request, author_id):
+    return HttpResponse("This is the authors/aid/inbox/ endpoint")
