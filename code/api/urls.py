45af944953b016426cd9dada0225b0662a274801
Diego Becerra <diego.enrique.becerra@gmail.com>
2021-10-16 13:52:53 -0600

    init all basic routes for api

--- code/api/urls.py
+++ code/api/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('authors/', views.authors, name='authors'),
-    path('authors/<author_id>/', views.author, name='author'),
-    path('authors/<author_id>/followers/', views.followers, name='followers'),
-    path('authors/<author_id>/liked/', views.liked, name='liked'),
-    path('authors/<author_id>/posts/<post_id>/', views.post, name='post'),
-    path('authors/<author_id>/posts/<post_id>/likes/', views.post_likes, name='post_likes'),
-    path('authors/<author_id>/posts/<post_id>/comments/', views.post_comments, name='post_comments'),
-    path('authors/<author_id>/posts/<post_id>/comments/<comment_id>/likes/', views.comment_likes, name='comment_likes'),
-    path('authors/<author_id>/inbox/', views.inbox, name='inbox'),
+    path('', index, name='index'),
+    path('authors/', AuthorsView.as_view(), name='authors'),
+    path('authors/<author_id>/', AuthorView.as_view(), name='author'),
+    path('authors/<author_id>/followers/', FollowersView.as_view(), name='followers'),
+    path('authors/<author_id>/liked/', LikedView.as_view(), name='liked'),
+    path('authors/<author_id>/posts/<post_id>/', PostView.as_view(), name='post'),
+    path('authors/<author_id>/posts/<post_id>/likes/', PostLikesView.as_view(), name='post_likes'),
+    path('authors/<author_id>/posts/<post_id>/comments/', PostCommentsView.as_view(), name='post_comments'),
+    path('authors/<author_id>/posts/<post_id>/comments/<comment_id>/likes/', CommentLikesView.as_view(), name='comment_likes'),
+    path('authors/<author_id>/inbox/', InboxView.as_view(), name='inbox'),
 ]
--- code/api/views.py
+++ code/api/views.py
@@ -1,81 +1,113 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.core import serializers
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 from socialDistribution.models import *
 
-# Create your views here.
+# https://docs.djangoproject.com/en/3.2/topics/class-based-views/intro/
 
 
 def index(request):
     return HttpResponse("Welcome to the Social Distribution API")
 
 
-def authors(request):
-    page = request.GET.get("page")
-    size = request.GET.get("size")
+@method_decorator(csrf_exempt, name='dispatch')
+class AuthorsView(View):
 
-    data = []
-    for author in Author.objects.all():
-        data.append(
-            {
-                # WARNING: hardcode
-                "type": "author",
-                "id": f"http://127.0.0.1:8000/authors/{author.id}",
-                "url": f"http://127.0.0.1:8000/author/{author.id}",
-                "host": "http://127.0.0.1:8000/",
-                "displayName": author.displayName,
-                "github": author.githubUrl,
-                "profileImage": "https://i.imgur.com/k7XVwpB.jpeg",
-            }
-        )
+    def get(self, request):
+        page = request.GET.get("page")
+        size = request.GET.get("size")
 
-    response = {
-        "type": "authors",
-        "items": data
-    }
+        data = []
+        for author in Author.objects.all():
+            data.append(
+                {
+                    # WARNING: hardcode
+                    "type": "author",
+                    "id": f"http://127.0.0.1:8000/authors/{author.id}",
+                    "url": f"http://127.0.0.1:8000/author/{author.id}",
+                    "host": "http://127.0.0.1:8000/",
+                    "displayName": author.displayName,
+                    "github": author.githubUrl,
+                    "profileImage": "https://i.imgur.com/k7XVwpB.jpeg",
+                }
+            )
 
-    return JsonResponse(response)
+        response = {
+            "type": "authors",
+            "items": data
+        }
 
-
-def author(request, author_id):
-    author = get_object_or_404(Author, pk=author_id)
-    response = {
-        # WARNING; hardcode
-        "type": "author",
-        "id": f"http://127.0.0.1:8000/authors/{author.id}",
-        "url": f"http://127.0.0.1:8000/author/{author.id}",
-        "host": "http://127.0.0.1:8000/",
-        "displayName": author.displayName,
-        "github": author.githubUrl,
-        "profileImage": "https://i.imgur.com/k7XVwpB.jpeg",
-    }
-
-    return JsonResponse(response)
+        return JsonResponse(response)
 
 
-def followers(request, author_id):
-    return HttpResponse("This is the authors/aid/followers/ endpoint")
+@method_decorator(csrf_exempt, name='dispatch')
+class AuthorView(View):
+
+    def get(self, request, author_id):
+        author = get_object_or_404(Author, pk=author_id)
+        response = {
+            # WARNING; hardcode
+            "type": "author",
+            "id": f"http://127.0.0.1:8000/authors/{author.id}",
+            "url": f"http://127.0.0.1:8000/author/{author.id}",
+            "host": "http://127.0.0.1:8000/",
+            "displayName": author.displayName,
+            "github": author.githubUrl,
+            "profileImage": "https://i.imgur.com/k7XVwpB.jpeg",
+        }
+        return JsonResponse(response)
+
+    def post(self, request, author_id):
+        return HttpResponse("authors post\nupdate profile")
 
 
-def liked(request, author_id):
-    return HttpResponse("This is the authors/aid/liked/ endpoint")
+@method_decorator(csrf_exempt, name='dispatch')
+class FollowersView(View):
+
+    def get(self, request, author_id):
+        return HttpResponse("This is the authors/aid/followers/ endpoint")
 
 
-def post(request, author_id, post_id):
-    return HttpResponse("This is the authors/aid/posts/pid/ endpoint")
+@method_decorator(csrf_exempt, name='dispatch')
+class LikedView(View):
+
+    def get(self, request, author_id):
+        return HttpResponse("This is the authors/aid/liked/ endpoint")
 
 
-def post_likes(request, author_id, post_id):
-    return HttpResponse("This is the authors/aid/posts/pid/likes/ endpoint")
+@method_decorator(csrf_exempt, name='dispatch')
+class PostView(View):
+
+    def get(self, request, author_id, post_id):
+        return HttpResponse("This is the authors/aid/posts/pid/ endpoint")
 
 
-def post_comments(request, author_id, post_id):
-    return HttpResponse("This is the authors/aid/posts/pid/comments/ endpoint")
+@method_decorator(csrf_exempt, name='dispatch')
+class PostLikesView(View):
+
+    def get(self, request, author_id, post_id):
+        return HttpResponse("This is the authors/aid/posts/pid/likes/ endpoint")
 
 
-def comment_likes(request, author_id, post_id, comment_id):
-    return HttpResponse("This is the authors/aid/posts/pid/comments/cid/likes/ endpoint")
+@method_decorator(csrf_exempt, name='dispatch')
+class PostCommentsView(View):
+
+    def get(self, request, author_id, post_id):
+        return HttpResponse("This is the authors/aid/posts/pid/comments/ endpoint")
 
 
-def inbox(request, author_id):
-    return HttpResponse("This is the authors/aid/inbox/ endpoint")
+@method_decorator(csrf_exempt, name='dispatch')
+class CommentLikesView(View):
+
+    def get(self, request, author_id, post_id, comment_id):
+        return HttpResponse("This is the authors/aid/posts/pid/comments/cid/likes/ endpoint")
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class InboxView(View):
+
+    def get(self, request, author_id):
+        return HttpResponse("This is the authors/aid/inbox/ endpoint")
