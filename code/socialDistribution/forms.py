c6f02445065ec8a7c83a89d0f3eb04f70e485212
Olivier Vadiavaloo <vadiaval@ualberta.ca>
2021-10-20 19:55:06 -0400

    Add textarea widget

--- code/socialDistribution/forms.py
+++ code/socialDistribution/forms.py
@@ -14,7 +14,8 @@ class CreateUserForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name']
 
-
+# MDN Web Docs, "Django Tutorial Part 9: Working with forms",
+# https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Forms, 2021-10-15, CC BY-SA 2.5
 class PostForm(forms.Form):
     """
         Create Post Form Configuration
@@ -22,7 +23,11 @@ class PostForm(forms.Form):
     title = forms.CharField(max_length=Post.TITLE_MAXLEN, required=True)
     categories = forms.CharField(required=False)
     description = forms.CharField(max_length=Post.DESCRIPTION_MAXLEN, required=True)
-    content_text = forms.CharField(max_length=Post.CONTEXT_TEXT_MAXLEN, required=False)
+    content_text = forms.CharField(
+        max_length=Post.CONTEXT_TEXT_MAXLEN, 
+        required=False,
+        widget=forms.Textarea
+    )
     content_media = forms.FileField(required=False)
     unlisted = forms.BooleanField(required=False)
     visibility = forms.ChoiceField(
