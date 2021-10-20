741a2e78b86d258acf442fd2012be5cc2e4816fb
Diego Becerra <diego.enrique.becerra@gmail.com>
2021-10-20 10:55:44 -0600

    Add functionalitiy for followers

--- code/api/views.py
+++ code/api/views.py
@@ -20,12 +20,13 @@ from .decorators import authenticate_request
 # Need to disable CSRF to make POST, PUT, etc requests. Otherwise, your request needs to contain 'X--CSRFToken: blahblah' with a CSRF token.
 # If we need CSRF validation in the future, just remove the csrf_exempt decorators.
 #
-# Martijn ten Hoor, https://stackoverflow.com/users/6945548/martijn-ten-hoor, "How to disable Django's CSRF validation?", 
+# Martijn ten Hoor, https://stackoverflow.com/users/6945548/martijn-ten-hoor, "How to disable Django's CSRF validation?",
 # 2016-10-12, https://stackoverflow.com/a/39993384, CC BY-SA 3.0
 #
 # Note: @ensure_crsf_cookie will send the token in the response
-# Ryan Pergent, https://stackoverflow.com/users/3904557/ryan-pergent, "how do I use ensure_csrf_cookie?", 
+# Ryan Pergent, https://stackoverflow.com/users/3904557/ryan-pergent, "how do I use ensure_csrf_cookie?",
 # 2017-05-30, https://stackoverflow.com/a/43712324, CC BY-SA 3.0
+
 
 def index(request):
     return HttpResponse("Welcome to the Social Distribution API")
@@ -34,10 +35,10 @@ def index(request):
 @method_decorator(csrf_exempt, name='dispatch')
 class AuthorsView(View):
 
-    """ GET: Retrieve all user profiles
-    """
     @method_decorator(ensure_csrf_cookie)
     def get(self, request):
+        """ GET - Retrieve all user profiles """
+
         page = request.GET.get("page")
         size = request.GET.get("size")
 
@@ -46,8 +47,8 @@ class AuthorsView(View):
             data.append(
                 {
                     "type": "author",
-                    "id": f"{HOST}{API_PREFIX}authors/{author.id}",
-                    "url": f"{HOST}{API_PREFIX}authors/{author.id}",
+                    "id": f"{HOST}{API_PREFIX}authors/{author.id}/",
+                    "url": f"{HOST}{API_PREFIX}authors/{author.id}/",
                     "host": f"{HOST}",
                     "displayName": author.displayName,
                     "github": author.githubUrl,
@@ -66,14 +67,14 @@ class AuthorsView(View):
 @method_decorator(csrf_exempt, name='dispatch')
 class AuthorView(View):
 
-    """ GET: Retrieve profile of {author_id} 
-    """
     def get(self, request, author_id):
+        """ GET - Retrieve profile of {author_id} """
+
         author = get_object_or_404(Author, pk=author_id)
         response = {
             "type": "author",
-            "id": f"{HOST}{API_PREFIX}authors/{author.id}",
-            "url": f"{HOST}{API_PREFIX}authors/{author.id}",
+            "id": f"{HOST}{API_PREFIX}authors/{author.id}/",
+            "url": f"{HOST}{API_PREFIX}authors/{author.id}/",
             "host": f"{HOST}",
             "displayName": author.displayName,
             "github": author.githubUrl,
@@ -81,9 +82,8 @@ class AuthorView(View):
         }
         return JsonResponse(response)
 
-    """ POST: Update profile of {author_id}
-    """
     def post(self, request, author_id):
+        """ POST - Update profile of {author_id} """
         return HttpResponse("authors post\nupdate profile")
 
 
@@ -91,7 +91,27 @@ class AuthorView(View):
 class FollowersView(View):
 
     def get(self, request, author_id):
-        return HttpResponse("This is the authors/aid/followers/ endpoint")
+        """ GET - Get a list of authors who are the followers of {author_id} """
+
+        author = get_object_or_404(Author, pk=author_id)
+        followers = []
+        for follower in author.followers.all():
+            followers.append({
+                "type": "author",
+                "id": f"{HOST}{API_PREFIX}authors/{follower.id}/",
+                "url": f"{HOST}{API_PREFIX}authors/{follower.id}/",
+                "host": f"{HOST}",
+                "displayName": follower.displayName,
+                "github": follower.githubUrl,
+                "profileImage": "https://i.imgur.com/k7XVwpB.jpeg",
+            })
+
+        response = {
+            "type": "followers",
+            "items": followers
+        }
+
+        return JsonResponse(response)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -132,43 +152,46 @@ class CommentLikesView(View):
 @method_decorator(csrf_exempt, name='dispatch')
 class InboxView(View):
 
-    """ GET: If authenticated, get a list of posts sent to {author_id}
-    """
     @method_decorator(authenticate_request)
     def get(self, request, author_id):
+        """ GET - If authenticated, get a list of posts sent to {author_id} """
+
         return JsonResponse({
             "message": f"This is the inbox for author_id={author_id}. Only author {author_id} can read this."
         })
 
-    """ POST: Send a post to {author_id}
-        - if the type is “post” then add that post to the author’s inbox
-        - if the type is “follow” then add that follow is added to the author’s inbox to approve later
-        - if the type is “like” then add that like to the author’s inbox    
-    """
     def post(self, request, author_id):
+        """ POST - Send a post to {author_id}
+            - if the type is “post” then add that post to the author’s inbox
+            - if the type is “follow” then add that follow is added to the author’s inbox to approve later
+            - if the type is “like” then add that like to the author’s inbox    
+        """
+
         data = json.loads(request.body)
         try:
             if data["type"] == "post":
-                return HttpResponse(status=501) # not implemented
+                return HttpResponse(status=501)  # not implemented
 
             elif data["type"] == "follow":
                 actor, obj = data["actor"], data["object"]
                 # actor want's to follow object
                 # need to add actor URL to object's inbox
                 # object decides later to add actor URL to its outward feed (followers)
-                response_data = str(data["actor"]) + "\n" + str(data["object"]) # demo, try with spec sample data
-                return HttpResponse(response_data) # okay
+                # demo, try with spec sample data
+                response_data = str(data["actor"]) + "\n" + str(data["object"])
+                return HttpResponse(response_data)  # okay
 
             elif data["type"] == "like":
-                return HttpResponse(status=501) # not implemented
-            
-            else: 
+                return HttpResponse(status=501)  # not implemented
+
+            else:
                 return HttpResponseBadRequest()
 
         except KeyError:
             return HttpResponseBadRequest()
-    
-    """ DELETE: Clear the inbox
-    """
+
+
     def delete(self, request, author_id):
+        """ DELETE - Clear the inbox """
+
         return HttpResponse("Hello")
