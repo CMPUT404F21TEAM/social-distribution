5e08974f802bbba292bedda90ce3fa87b760e1a0
Jawad Hossain <jhossain@ualberta.ca>
2021-10-23 15:04:17 -0600

    #89 Add some comment model tests

--- code/socialDistribution/tests.py
+++ code/socialDistribution/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from mixer.backend.django import mixer
+from datetime import timedelta
 
+
+from .models import *
 # Create your tests here.
+
+class CommentModelTests(TestCase):
+
+    def test_when_just_now (self):
+        '''
+            comment.when() returns just now right after post creation
+        '''
+        author = mixer.blend(Author)
+        post = mixer.blend(Post, author = author)
+        comment = mixer.blend(Comment, author=author, post=post, pub_date = datetime.now(timezone.utc) )
+
+        self.assertIs( comment.when() == 'just now', True)
+
+    def test_when_10_seconds (self):
+        '''
+            comment.when() returns 10 seconds ago after the time has passed
+        '''
+        author = mixer.blend(Author)
+        post = mixer.blend(Post, author = author)
+
+        pub_date = datetime.now(timezone.utc) - timedelta(seconds=10)
+        comment = mixer.blend(Comment, author=author, post=post, pub_date = pub_date  )
+
+        self.assertIs( comment.when() == '10 seconds ago', True)
+
+
+
