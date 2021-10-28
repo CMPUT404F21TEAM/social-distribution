9d77fdd48f969136d1189cd8f70e4b311bae3984
Michelle Wang <44190626+mxiuwang@users.noreply.github.com>
2021-10-28 15:09:36 -0400

    Update markdown.js

--- code/socialDistribution/static/js/markdown.js
+++ code/socialDistribution/static/js/markdown.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     $(".content-markdown").each(function(){
       var content = $(this).text()
-      console.log("content",content)
       var markedContent = marked(content)
       $(this).html(markedContent)
     })
