ff68fbb4b0a799ce87cf1d396f8ddf053b251d39
Jawad Hossain <jhossain@ualberta.ca>
2021-11-06 14:32:26 -0600

    134 Fix typo and redirect without local author check

--- code/socialDistribution/static/js/comment.js
+++ code/socialDistribution/static/js/comment.js
@@ -1,8 +1,6 @@
     /**
-     * Redirect to home page of local author
+     * Redirect to home page of author
      * */
-     function redirectToAuhtorPage(author_type, id) {
-        if (author_type === "Local") {
-            location.href = `${location.protocol}//${location.host}/app/author/${id}`;
-        }
+     function redirectToAuthorPage(id) {
+        location.href = `${location.protocol}//${location.host}/app/author/${id}`;
     }
