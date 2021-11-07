57aaced6f41866ffc8ede6d6460fe02b0c7c2548
olivier-vadiaval <vadiaval@ualberta.ca>
2021-11-07 16:37:09 -0700

    Pass in user-agent header

--- code/socialDistribution/github_activity/github_activity.py
+++ code/socialDistribution/github_activity/github_activity.py
@@ -10,15 +10,18 @@ def get_event_description(event_name, event_data):
     github_event = event_factory.get_event(event_name)
     github_event.set_event_data(event_data)
     
-    return tuple(github_event.get_description(), github_event.time_ago())
+    return github_event.get_description(), github_event.time_ago()
 
 
 def pull_github_events(github_user):
     response = requests.get(
             REQUEST_URL.format(github_user = github_user),
-            headers={"Content-Type": "application/json; charset=UTF-8"}
+            headers={
+                "Content-Type": "application/json; charset=UTF-8",
+                "User-Agent": "request"
+                }
         )
-    
+
     if response.status_code == 200:
         events_json = response.json()
         return [get_event_description(
