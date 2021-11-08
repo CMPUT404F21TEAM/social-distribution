6a9e96e383f845286304227d3b078bc07064d943
olivier-vadiaval <vadiaval@ualberta.ca>
2021-11-07 21:08:45 -0700

    Rename github_event_card.html to github_event.html

--- code/socialDistribution/templatetags/github_event.py
+++ code/socialDistribution/templatetags/github_event.py
@@ -4,6 +4,6 @@ register = template.Library()
 
 # Django Software Foundation, "Custom Template tags and Filters", 2021-10-10
 # https://docs.djangoproject.com/en/3.2/howto/custom-template-tags/#inclusion-tags
-@register.inclusion_tag('tagtemplates/github_event_card.html')
+@register.inclusion_tag('tagtemplates/github_event.html')
 def card_gh_event(github_event):
     return {'github_event_descr': github_event[0], 'timeago': github_event[1]}
