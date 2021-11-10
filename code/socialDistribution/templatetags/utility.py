03f4e72ae0ff55319d48fe3473889d33cb232c37
Jawad Hossain <jhossain@ualberta.ca>
2021-11-09 19:05:03 -0700

    #146 Seperate Post Card and Form

--- code/socialDistribution/templatetags/utility.py
+++ code/socialDistribution/templatetags/utility.py
@@ -2,6 +2,8 @@ from django import template
 
 register = template.Library()
 
+# 'How to concatenate strings in django templates?'
+# https://stackoverflow.com/questions/4386168/how-to-concatenate-strings-in-django-templates/23783666#23783666 - Roger Dahl
 @register.filter
 def addstr(arg1, arg2):
     """concatenate arg1 & arg2"""
