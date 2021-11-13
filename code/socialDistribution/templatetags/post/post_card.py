cc808f3b5394d0b497319265197bfc1c295b73c4
olivier-vadiaval <vadiaval@ualberta.ca>
2021-11-13 13:44:38 -0700

    Add post remote check for liking

--- code/socialDistribution/templatetags/post/post_card.py
+++ code/socialDistribution/templatetags/post/post_card.py
@@ -1,6 +1,7 @@
 from django import template
 import base64
 from socialDistribution.forms import PostForm
+from socialDistribution.models.post import LocalPost, InboxPost
 
 register = template.Library()
 
@@ -13,26 +14,30 @@ def post_card(post, author):
     """
 
     # Delete/Edit
-    isAuthor = post.author == author
-    isPublic = post.is_public()
+    is_author = post.author == author
+    is_public = post.is_public()
+    if type(post) is InboxPost:
+        post_host = 'remote'
+    else:
+        post_host = 'local'
 
     # Likes
-    isLiked = False # temp post.likes.filter(author=author).exists()
-    likeText = ''
+    is_liked = False # temp post.likes.filter(author=author).exists()
+    like_text = ''
     likes = 0 #temp post.total_likes()
-    if isLiked:
+    if is_liked:
         likes -= 1
         if likes >= 2:
-            likeText = f'Liked by you and {likes} others'
+            like_text = f'Liked by you and {likes} others'
         elif likes == 1:
-            likeText = f'Liked by you and 1 other'
+            like_text = f'Liked by you and 1 other'
         else:
-            likeText = f'Liked by you'
+            like_text = f'Liked by you'
     else:
         if likes > 1:
-            likeText = f'Liked by {likes} others'
+            like_text = f'Liked by {likes} others'
         elif likes == 1:
-            likeText = f'Liked by 1 other'
+            like_text = f'Liked by 1 other'
 
     content_media = None
     # if post.content_media is not None:
@@ -40,9 +45,10 @@ def post_card(post, author):
 
     return {
         'post': post, 
-        'content_media': content_media, 
-        'isAuthor': isAuthor, 
-        'isLiked': isLiked, 
-        'likeText': likeText,        
-        'isPublic': isPublic,
+        'content_media': content_media,
+        'post_host': post_host,
+        'is_author': is_author, 
+        'is_liked': is_liked, 
+        'like_text': like_text,        
+        'is_public': is_public,
         }
