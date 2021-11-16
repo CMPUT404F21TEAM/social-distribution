061384525a5a634e71aff10b4d8906bb9dd35d9a
olivier-vadiaval <vadiaval@ualberta.ca>
2021-11-16 11:29:12 -0700

    Move likes GET request to utility

--- code/socialDistribution/templatetags/post/post_card.py
+++ code/socialDistribution/templatetags/post/post_card.py
@@ -2,7 +2,7 @@ from django import template
 import base64
 from socialDistribution.forms import PostForm
 from socialDistribution.models.post import LocalPost, InboxPost
-from socialDistribution.utility import make_request
+from socialDistribution.utility import get_post_like_info, get_like_text
 
 register = template.Library()
 
@@ -19,46 +19,13 @@ def post_card(post, author):
     is_public = post.is_public()
     is_friends = post.is_friends()
 
-    # if the post is remote, need to fetch likes
     if type(post) is InboxPost:
         post_host = 'remote'
-        request_url = post.public_id.strip('/') + '/likes'
-        response = make_request('GET', request_url)
-
-        if response.status_code == 200:
-            likes_list = response.json()
-
-            for like in likes_list:
-                if like['author']['id'] == author.get_url_id():
-                    is_liked = True
-                    break
-
-            likes = len(likes_list)
-
-        else:
-            is_liked = None
-            likes = 0
-
     else:
         post_host = 'local'
-        is_liked = post.likes.filter(author=author).exists()
-        likes = post.total_likes()
-
-    # Likes
-    like_text = ''
-    if is_liked:
-        likes -= 1
-        if likes >= 2:
-            like_text = f'Liked by you and {likes} others'
-        elif likes == 1:
-            like_text = f'Liked by you and 1 other'
-        else:
-            like_text = f'Liked by you'
-    else:
-        if likes > 1:
-            like_text = f'Liked by {likes} others'
-        elif likes == 1:
-            like_text = f'Liked by 1 other'
+    
+    is_liked, likes = get_post_like_info(post, author)
+    like_text = get_like_text(is_liked, likes)
 
     content_media = None
     # if post.content_media is not None:
