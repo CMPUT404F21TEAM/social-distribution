b620bb6556d56b9b5f6676f976809922ccbe3508
Diego Becerra <47841864+debecerra@users.noreply.github.com>
2021-11-17 21:10:38 -0700

    19 - Befriend Remote Authors, 16 - Post to my "stream" (#169)
    * Add Follow model
    
    * Add follow methods to LocalAuthor
    
    * Fix Follow.is_friend() method
    
    * Clean up inbox API

--- code/socialDistribution/models/__init__.py
+++ code/socialDistribution/models/__init__.py
@@ -8,3 +8,4 @@ from .post import LocalPost, InboxPost
 from .category import Category
 from .comment import Comment
 from .like import PostLike, CommentLike
+from .follow import Follow
---
+++ code/socialDistribution/models/follow.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+from socialDistribution import requests as api_requests
+
+
+class Follow(models.Model):
+    object = models.ForeignKey('LocalAuthor', on_delete=models.CASCADE, related_name="follows")
+    actor = models.ForeignKey('Author', on_delete=models.CASCADE, related_name="following")
+
+    class Meta:
+        # Django Software Foundation, "UniqueConstraint", 2021-11-06,
+        # https://docs.djangoproject.com/en/3.2/ref/models/constraints/#uniqueconstraint
+        constraints = [
+            # do not allow actor to follow same object more than once
+            models.UniqueConstraint(fields=['actor', 'object'], name='unique_follow'),
+        ]
+
+    def is_friend(self):
+        # make api request
+        actor_url = self.actor.url.strip('/')
+        object_url = self.object.url.strip('/')
+        endpoint = actor_url + '/followers/' + object_url
+        response_body = api_requests.get(endpoint)
+
+        # check if GET request came back with author object
+        if response_body and response_body.get("id") == object_url:
+            return True 
+        else:
+            return False
