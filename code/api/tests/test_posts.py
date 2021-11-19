23ec281ddae74ac57d41c06f1cbe329c2349df38
Tyler Elston <24640479+tylerelston@users.noreply.github.com>
2021-11-19 15:12:33 -0700

    GET pagination #141 (#168)

--- code/api/tests/test_posts.py
+++ code/api/tests/test_posts.py
@@ -7,20 +7,27 @@ from django.utils import timezone
 from mixer.backend.django import mixer
 import json
 
-from socialDistribution.models import LocalPost, Category
+from socialDistribution.models import LocalPost, Category, LocalAuthor
 from .test_authors import create_author
 from cmput404.constants import HOST, API_PREFIX
 from datetime import datetime
+
+def create_post(title, author):
+    return LocalPost.objects.create(
+            author_id=author.id,
+            title=title,
+            description="testDesc",
+            content_type=LocalPost.ContentType.PLAIN,
+            content="testContexxt",
+            visibility=LocalPost.Visibility.PUBLIC,
+            unlisted=False,
+        )
 
 
 def get_post_json(post):
     previousCategories = post.categories.all()
     previousCategoriesNames = [cat.category for cat in previousCategories]
     return {
-        "type":"posts",
-        "page": None,
-        "size": None,
-        "items": [{
             "type":"post",
             "title":post.title,
             "id": f"http://{HOST}/{API_PREFIX}/author/{post.author.id}/posts/{post.id}",
@@ -37,16 +44,60 @@ def get_post_json(post):
             "published":post.published.isoformat(),
             "visibility":post.get_visibility_display(),
             "unlisted":post.unlisted
-        }]
-            }
+        }
 
 class PostsViewTest(TestCase):
 
     def test_get_posts_basic(self):
         self.maxDiff = None
-        post = mixer.blend(LocalPost, content_type='PL')
-        expected = get_post_json(post)
+        author = mixer.blend(LocalAuthor)
+        post = create_post("first", author)
+        expected = {
+            "type":"posts",
+            "page": None,
+            "size": None,
+            "items": [get_post_json(post)]
+            }
 
         response = self.client.get(reverse('api:posts', args=(post.author.id,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, expected)
+        
+    def test_get_posts_paginated(self):
+        self.maxDiff = None
+        page = 1
+        size = 2
+        author = mixer.blend(LocalAuthor)
+        post1 = create_post("first", author)
+        post2 = create_post("second", author)
+
+        expected = {
+                "type": "posts",
+                "page": page,
+                "size": size,
+                "items": [get_post_json(post1), get_post_json(post2)]
+            }
+        
+        response = self.client.get(reverse('api:posts', args=(post1.author.id,)) + f'?page={page}&size={size}')
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, expected)
+        
+    def test_get_posts_paginated_page_2(self):
+        self.maxDiff = None
+        page = 2
+        size = 2
+        author = mixer.blend(LocalAuthor)
+        post1 = create_post("first", author)
+        post2 = create_post("second", author)
+        post3 = create_post("third", author)
+
+        expected = {
+                "type": "posts",
+                "page": page,
+                "size": size,
+                "items": [get_post_json(post3)]
+            }
+        
+        response = self.client.get(reverse('api:posts', args=(post1.author.id,)) + f'?page={page}&size={size}')
         self.assertEqual(response.status_code, 200)
         self.assertJSONEqual(response.content, expected)
