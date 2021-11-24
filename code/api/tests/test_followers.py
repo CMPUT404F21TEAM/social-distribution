43b6026cf9ad0b1bbd5046602564b3178e6bdcf6
Diego Becerra <diego.enrique.becerra@gmail.com>
2021-11-23 21:32:22 -0700

    remove test logging

--- code/api/tests/test_followers.py
+++ code/api/tests/test_followers.py
@@ -96,6 +96,19 @@ class FollowersSingleViewTests(LiveServerTestCase):
 
 class FollowersViewTests(TestCase):
 
+    # the pillow, https://stackoverflow.com/users/2812257/the-pillow, "How can I disable logging while running unit tests in Python Django?"
+    # https://stackoverflow.com/a/54519433, 2019-02-04, CC BY-SA 4.0
+
+    # disable logging before tests
+    @classmethod
+    def setUpClass(cls):
+        logging.disable(logging.CRITICAL)
+
+    # enable logging after tests
+    @classmethod
+    def tearDownClass(cls):
+        logging.disable(logging.NOTSET)
+
     def test_get_followers(self):
 
         author = mixer.blend(LocalAuthor)
