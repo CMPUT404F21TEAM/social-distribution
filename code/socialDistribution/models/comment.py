b24080e9818a6cb9df58d528cc41dbda418f5f47
Jawad Hossain <jhossain@ualberta.ca>
2021-11-29 22:37:27 -0700

    #241 Increase comment char field len

--- code/socialDistribution/models/comment.py
+++ code/socialDistribution/models/comment.py
@@ -16,6 +16,8 @@ class Comment(models.Model):
         post                Post related to the comment (reference to post)
         likes               Likes created by Authors that liked this comment
     '''
+    STRING_MAXLEN = 255
+
     class CommentContentType(models.TextChoices):
         PLAIN = 'PL', 'text/plain'
         MARKDOWN = 'MD', 'text/markdown'
@@ -25,10 +27,11 @@ class Comment(models.Model):
 
     author = models.ForeignKey('Author', on_delete=models.CASCADE)
     content_type = models.CharField(
-        max_length=2,
-        choices=CommentContentType.choices
+        max_length=STRING_MAXLEN,
+        choices=CommentContentType.choices,
+        default=CommentContentType.PLAIN
     )
-    comment = models.CharField(max_length=200)
+    comment = models.CharField(max_length=STRING_MAXLEN)
 
     post = models.ForeignKey('LocalPost', on_delete=models.CASCADE)
     pub_date = models.DateTimeField()
