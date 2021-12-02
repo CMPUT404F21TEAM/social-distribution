1759e7fd9aeeac684f838ac4faeb1d8dcd8aa1e4
Diego Becerra <diego.enrique.becerra@gmail.com>
2021-12-02 10:34:58 -0700

    Reduce parallel db connections for follow updates

--- code/socialDistribution/views.py
+++ code/socialDistribution/views.py
@@ -11,7 +11,7 @@ from django.shortcuts import redirect
 from django.db.models import Count, Q
 
 from cmput404.constants import SCHEME, HOST, API_BASE, LOCAL, REMOTE
-from socialDistribution.fetchers import fetch_remote_authors, fetch_author_update
+from socialDistribution.fetchers import fetch_remote_authors, fetch_author_update, fetch_follow_update
 from .forms import CreateUserForm, PostForm
 
 import base64
@@ -373,6 +373,8 @@ def author(request, author_id):
 
     # send update signal
     fetch_author_update(author)
+    fetch_follow_update(curr_user, author)
+    fetch_follow_update(author, curr_user)
 
     if author_type == LOCAL:
         posts = author.posts.listed().get_public()  # get public posts only
