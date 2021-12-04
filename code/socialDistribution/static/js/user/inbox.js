e62a094a557356e47f1cdf1afb1673e6ec81fe4d
olivier-vadiaval <vadiaval@ualberta.ca>
2021-12-04 13:44:21 -0700

    Revert previous commit. Fix empty inbox on update

--- code/socialDistribution/static/js/user/inbox.js
+++ code/socialDistribution/static/js/user/inbox.js
@@ -15,20 +15,9 @@ function update() {
         type: "GET",
         success: function (data) {
             console.log('Success');
-            let new_requests = $(data).find('.follow-requests');
-            let new_posts = $(data).find('.inbox-posts');
 
-            let empty_inbox = $(data).find('.empty-inbox');
-            let current_empty_inbox = $(document).find('.empty-inbox');
-
-            if (empty_inbox.length === 0 && current_empty_inbox.length !== 0)
-                current_empty_inbox.remove();
-
-            // Update follow requests
-            $('.follow-requests').html(new_requests.html());
-
-            // Update posts
-            $('.inbox-posts').html(new_posts.html());
+            let inbox = $(data).find('.inbox').html();
+            $('.inbox').html(inbox);
 
             configEditPostModal()
             handleMarkDown();
