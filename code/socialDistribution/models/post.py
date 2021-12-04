2a65bbcf3d335d0e13e0e1cc497a8865c62f0882
Diego Becerra <diego.enrique.becerra@gmail.com>
2021-12-04 11:33:43 -0700

    add FRIENDS to accepted visibility content

--- code/socialDistribution/models/post.py
+++ code/socialDistribution/models/post.py
@@ -70,7 +70,7 @@ class Post(models.Model):
             if visibility.upper() == "PUBLIC" or visibility.upper() == "PB":
                 return cls.PUBLIC
 
-            elif visibility.upper() == "FRIEND" or visibility.upper() == "FR":
+            elif visibility.upper() == "FRIEND" or visibility.upper() == "FR" or visibility.upper() == "FRIENDS":
                 return cls.FRIENDS
 
             elif visibility.upper() == "PRIVATE" or visibility.upper() == "PR":
