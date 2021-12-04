3aea59409cb7cef58ace1551dc6cab872e6d40ce
olivier-vadiaval <vadiaval@ualberta.ca>
2021-12-04 13:34:41 -0700

    Remove empty inbox text on update

--- code/socialDistribution/static/js/user/inbox.js
+++ code/socialDistribution/static/js/user/inbox.js
@@ -15,14 +15,20 @@ function update() {
         type: "GET",
         success: function (data) {
             console.log('Success');
-            let new_requests = $(data).find('.follow-requests').html();
-            let new_posts = $(data).find('.inbox-posts').html();
+            let new_requests = $(data).find('.follow-requests');
+            let new_posts = $(data).find('.inbox-posts');
+
+            let empty_inbox = $(data).find('.empty-inbox');
+            let current_empty_inbox = $(document).find('.empty-inbox');
+
+            if (empty_inbox.length === 0 && current_empty_inbox.length !== 0)
+                current_empty_inbox.remove();
 
             // Update follow requests
-            $('.follow-requests').html(new_requests);
+            $('.follow-requests').html(new_requests.html());
 
             // Update posts
-            $('.inbox-posts').html(new_posts);
+            $('.inbox-posts').html(new_posts.html());
 
             configEditPostModal()
             handleMarkDown();
